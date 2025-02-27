67271c3ee180298356be42552fed6c5f6cc44d49
Pietro Max Marsella <marsella@stanford.edu>
2025-02-26 20:13:15 -0800

    update

--- lib/compiler/src/compiler/machine_mapping/allowed_machine_views.cc
+++ lib/compiler/src/compiler/machine_mapping/allowed_machine_views.cc
@@ -68,13 +68,19 @@ static std::unordered_set<MachineView>
     nonnegative_int max_stride_upper_bound =
         get_max_stride_upper_bound(tensor_dims, total_devices);
 
-    std::vector<stride_t> single_stride_range =
-        transform(nonnegative_range(1_n, max_stride_upper_bound + 1_n),
-                  [](nonnegative_int stride) { return stride_t{stride}; });
+    std::vector<std::vector<stride_t>> stride_options =
+        transform(tensor_dims, [&](nonnegative_int dim_size) {
+          if (dim_size != 1_n) {
+            return transform(
+                nonnegative_range(1_n, max_stride_upper_bound + 1_n),
+                [](nonnegative_int stride) { return stride_t{stride}; });
+          } else {
+            return std::vector<stride_t>{stride_t{1_n}};
+          }
+        });
+
     std::unordered_multiset<std::vector<stride_t>> raw_stride_vectors =
-        cartesian_product(
-            repeat_element(/*num_times=*/num_elements(tensor_dims),
-                           /*element=*/single_stride_range));
+        cartesian_product(stride_options);
     std::unordered_multiset<MultiDimensionalStride> strides =
         transform(raw_stride_vectors, [](auto const &stride_vec) {
           return MultiDimensionalStride{stride_vec};
@@ -96,10 +102,18 @@ static std::unordered_set<MachineView>
   };
 
   auto candidate_dimensions = [](OperatorTaskSpace const &task) {
-    std::unordered_set<MachineSpecificationDimension> options = {
-        MachineSpecificationDimension::INTER_NODE,
-        MachineSpecificationDimension::INTRA_NODE};
-    return get_all_permutations_with_repetition(options, num_dims(task));
+    std::vector<std::vector<MachineSpecificationDimension>> dimension_options =
+        transform(task.degrees, [](nonnegative_int dim_size) {
+          if (dim_size == 1_n) {
+            return std::vector<MachineSpecificationDimension>{
+                MachineSpecificationDimension::INTRA_NODE};
+          } else {
+            return std::vector<MachineSpecificationDimension>{
+                MachineSpecificationDimension::INTER_NODE,
+                MachineSpecificationDimension::INTRA_NODE};
+          }
+        });
+    return cartesian_product(dimension_options);
   };
 
   std::vector<nonnegative_int> tensor_dims = task.degrees;
--- lib/compiler/test/src/compiler/machine_mapping/allowed_machine_views.cc
+++ lib/compiler/test/src/compiler/machine_mapping/allowed_machine_views.cc
@@ -13,6 +13,22 @@ TEST_SUITE(FF_TEST_SUITE) {
 
   TEST_CASE("get_allowed_machine_views") {
 
+    auto make_2d_view = [&](nonnegative_int start_node_idx,
+                            nonnegative_int start_device_idx,
+                            nonnegative_int stride_1,
+                            nonnegative_int stride_2,
+                            MachineSpecificationDimension m1,
+                            MachineSpecificationDimension m2) {
+      return MachineView{
+          MachineSpaceCoordinate{
+              start_node_idx, start_device_idx, DeviceType::GPU},
+          {MachineViewDimension{stride_t{stride_1}, m1},
+           MachineViewDimension{stride_t{stride_2}, m2}},
+      };
+    };
+    auto intra = MachineSpecificationDimension::INTRA_NODE;
+    auto inter = MachineSpecificationDimension::INTER_NODE;
+
     SUBCASE("1 degree of parallelism") {
       MachineSpecification ms = MachineSpecification{
           /*num_nodes=*/1_n,
@@ -69,36 +85,20 @@ TEST_SUITE(FF_TEST_SUITE) {
       };
       OperatorTaskSpace task = OperatorTaskSpace{{2_n, 3_n}};
 
-      auto make_2d_view = [&](nonnegative_int start_node_idx,
-                              nonnegative_int start_device_idx,
-                              nonnegative_int stride1,
-                              nonnegative_int stride2,
-                              MachineSpecificationDimension m1,
-                              MachineSpecificationDimension m2) {
-        return MachineView{
-            MachineSpaceCoordinate{
-                start_node_idx, start_device_idx, DeviceType::GPU},
-            {MachineViewDimension{stride_t{stride1}, m1},
-             MachineViewDimension{stride_t{stride2}, m2}},
-        };
-      };
-
-      auto intra = MachineSpecificationDimension::INTRA_NODE;
-      auto inter = MachineSpecificationDimension::INTER_NODE;
       std::unordered_set<MachineView> correct = {
           make_2d_view(
-              0_n, 0_n, /*stride1=*/1_n, /*stride2=*/1_n, inter, intra),
+              0_n, 0_n, /*stride_1=*/1_n, /*stride_2=*/1_n, inter, intra),
           make_2d_view(
-              1_n, 0_n, /*stride1=*/1_n, /*stride2=*/1_n, inter, intra),
+              1_n, 0_n, /*stride_1=*/1_n, /*stride_2=*/1_n, inter, intra),
           make_2d_view(
-              0_n, 0_n, /*stride1=*/2_n, /*stride2=*/1_n, inter, intra),
+              0_n, 0_n, /*stride_1=*/2_n, /*stride_2=*/1_n, inter, intra),
 
           make_2d_view(
-              0_n, 0_n, /*stride1=*/1_n, /*stride2=*/1_n, intra, inter),
+              0_n, 0_n, /*stride_1=*/1_n, /*stride_2=*/1_n, intra, inter),
           make_2d_view(
-              0_n, 1_n, /*stride1=*/1_n, /*stride2=*/1_n, intra, inter),
+              0_n, 1_n, /*stride_1=*/1_n, /*stride_2=*/1_n, intra, inter),
           make_2d_view(
-              0_n, 0_n, /*stride1=*/2_n, /*stride2=*/1_n, intra, inter),
+              0_n, 0_n, /*stride_1=*/2_n, /*stride_2=*/1_n, intra, inter),
       };
 
       std::unordered_set<MachineView> result =
@@ -107,7 +107,7 @@ TEST_SUITE(FF_TEST_SUITE) {
       CHECK(correct == result);
     }
 
-    SUBCASE("4D operator task space") {
+    SUBCASE("2D operator task space, dimensions (1,1)") {
       MachineSpecification full_machine_spec = MachineSpecification{
           /*num_nodes=*/nonnegative_int{2},
           /*num_cpus_per_node=*/nonnegative_int{1},
@@ -115,12 +115,42 @@ TEST_SUITE(FF_TEST_SUITE) {
           /*inter_node_bandwidth=*/1,
           /*intra_node_bandwidth=*/1,
       };
-      OperatorTaskSpace task = OperatorTaskSpace{{1_n, 1_n, 1_n, 1_n}};
+      OperatorTaskSpace task = OperatorTaskSpace{{1_n, 1_n}};
 
       std::unordered_set<MachineView> result =
           get_allowed_machine_views(full_machine_spec, task, DeviceType::GPU);
 
-      CHECK(result.size() < 10);
+      std::unordered_set<MachineView> correct = {
+          make_2d_view(
+              0_n, 0_n, /*stride_1=*/1_n, /*stride_2=*/1_n, intra, intra),
+          make_2d_view(
+              1_n, 0_n, /*stride_1=*/1_n, /*stride_2=*/1_n, intra, intra)};
+      CHECK(correct == result);
+    }
+
+    SUBCASE("2D operator task space, dimensions (2,1)") {
+      MachineSpecification full_machine_spec = MachineSpecification{
+          /*num_nodes=*/nonnegative_int{2},
+          /*num_cpus_per_node=*/nonnegative_int{2},
+          /*num_gpus_per_node=*/nonnegative_int{2},
+          /*inter_node_bandwidth=*/1,
+          /*intra_node_bandwidth=*/1,
+      };
+      OperatorTaskSpace task = OperatorTaskSpace{{1_n, 2_n}};
+
+      std::unordered_set<MachineView> result =
+          get_allowed_machine_views(full_machine_spec, task, DeviceType::GPU);
+
+      std::unordered_set<MachineView> correct = {
+          make_2d_view(
+              0_n, 0_n, /*stride_1=*/1_n, /*stride_2=*/1_n, intra, intra),
+          make_2d_view(
+              0_n, 0_n, /*stride_1=*/1_n, /*stride_2=*/1_n, intra, inter),
+          make_2d_view(
+              1_n, 0_n, /*stride_1=*/1_n, /*stride_2=*/1_n, intra, intra),
+          make_2d_view(
+              0_n, 1_n, /*stride_1=*/1_n, /*stride_2=*/1_n, intra, inter)};
+      CHECK(correct == result);
     }
   }
 }
